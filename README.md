b0a8e00ff1a6cf85649eaefbb9dedc28385f55e1
Chandrakant shinde <chandrakantshinde7348@gmail.com>
2023-10-22 14:46:18 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# hiringpro
+Hiring Platform
---
+++ beekin-frontend/src/App.js
@@ -0,0 +1,12 @@
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <>
+      <h1>Code Here</h1>
+      <Home />
+    </>
+  );
+}
+
+export default App;
